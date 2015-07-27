e1babafbc8150e973461e4efde16839bb9a35758
accunite <accunite@users.noreply.github.com>
2015-07-27 14:42:13 +0530

    testrepo.php

--- testrepo.php
+++ testrepo.php
@@ -1,10 +1,52 @@
-ddsfdsa 
-asdasd
-asd
-saasda
-dscasdc
-gdfgdfg
-sdfsdgesf
-sdf
-sdfffffffffsdf
-sdffffff
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+	<meta charset="utf-8" />
+
+	<title>BuySellBannerAds</title>
+<link rel="shortcut icon" type="image/x-icon" href="tbuysellbannerads.ico" />
+	<!-- CSS -->
+	<link rel="stylesheet" href="1WD-coming-soon-page/css/reset.css">
+	<link rel="stylesheet" href="1WD-coming-soon-page/fonts/stylesheet.css">
+	<link rel="stylesheet" href="1WD-coming-soon-page/css/style.css">
+
+	<!--[if lt IE 9]>
+		<link rel="stylesheet" href="css/ie.css">
+	<![endif]-->
+
+	<!-- IE fix for HTML5 tags -->
+	<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!-- jQuery and Modernizr-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script src="1WD-coming-soon-page/js/modernizr.custom.js"></script>
+
+	<!-- Countdown timer and other animations -->
+	<script src="1WD-coming-soon-page/js/jquery.countdown.js"></script>
+	<script src="1WD-coming-soon-page/js/script.js"></script>
+
+</head>
+
+<body>
+<img src="buysellbannerads.png" width="250px;">
+	<header>
+		<h1 align='center'>We are coming soon..</h1>
+		
+	</header>
+
+	<div id="main">
+		[ad id='3170']
+
+		<div id="counter"></div>
+
+		
+	</div>
+
+</body>
+
+</html>
